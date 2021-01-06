6b676df5364a1b3501aa0908ca52cf3e0e9b74b4
f3d0t <logrus7@gmail.com>
2021-01-06 23:01:52 +0300

    final

--- friends-app/js/app.js
+++ friends-app/js/app.js
@@ -1,9 +1,9 @@
 const MAIN = document.querySelector(".main");
+const FILTERS = document.querySelector(".filters");
+const RESET_BUTTON = document.querySelector(".filters__button");
 
-const FRIENDS_COUNT = 50;
+const FRIENDS_COUNT = 60;
 const REQUEST_LINK = `https://randomuser.me/api/?results=${FRIENDS_COUNT}`;
-
-//const FRIENDS_DATA = [];
 
 const fetchData = async (requestLink) => {
 	try {
@@ -15,16 +15,101 @@ const fetchData = async (requestLink) => {
 	}
 };
 
+const initApp = (dataArray) => {
+	const friends = new FriendsList(dataArray, MAIN);
+	friends.renderCards();
+	bindEventListeners(friends);
+};
+
+const bindEventListeners = (friends) => {
+	FILTERS.addEventListener("input", ({ target }) => {
+		if (target.name == "search") {
+			friends.filterBySearch(target.value);
+		}
+		if (target.name == "gender") {
+			friends.filterByGender(target.id);
+			FILTERS.elements.search.value = "";
+		}
+		if (target.name == "name") {
+			friends.sortByName(target.id);
+			FILTERS.elements.search.value = "";
+		}
+		if (target.name == "age") {
+			friends.sortByAge(target.id);
+			FILTERS.elements.search.value = "";
+		}
+	});
+	RESET_BUTTON.addEventListener("click", (e) => {
+		e.preventDefault();
+		FILTERS.querySelectorAll("input[type=radio]").forEach((radioButton) => {
+			radioButton.checked = false;
+		});
+		FILTERS.elements.search.value = "";
+		friends.resetFilters();
+	});
+	FILTERS.addEventListener("keydown", (event) => {
+		if (event.keyIdentifier == "U+000A" || event.keyIdentifier == "Enter" || event.keyCode == 13) {
+			event.preventDefault();
+			return false;
+		}
+	});
+};
+
 class FriendsList {
 	constructor(data, parentWrapper) {
-		this.cards = data.map((friendData) => new FriendCard(friendData));
+		this.allCards = data.map((friendData) => new FriendCard(friendData));
+		this.currentCards = this.allCards;
 		this.wrapper = parentWrapper;
 	}
-	renderCards() {
+	renderCards(cardsArray = this.currentCards) {
 		this.wrapper.innerHTML = "";
-		this.wrapper.append(...this.cards.map((card) => card.createCard()));
+		this.wrapper.append(...cardsArray.map((card) => card.createCard()));
+	}
+	resetFilters() {
+		this.currentCards = this.allCards;
+		this.renderCards();
+	}
+	filterByGender(gender) {
+		if (gender == "male") {
+			this.currentCards = this.allCards.filter((friendCard) => friendCard["gender"] == "male");
+		}
+		if (gender == "female") {
+			this.currentCards = this.allCards.filter((friendCard) => friendCard["gender"] == "female");
+		}
+		if (gender == "all") {
+			this.currentCards = this.allCards;
+		}
+		this.renderCards();
+	}
+	sortByName(sortType) {
+		if (sortType == "az") {
+			this.currentCards = sortObjectsByPropertyValue(this.currentCards, "name", "ascending");
+		}
+		if (sortType == "za") {
+			this.currentCards = sortObjectsByPropertyValue(this.currentCards, "name", "descending");
+		}
+		this.renderCards();
+	}
+	sortByAge(sortType) {
+		if (sortType == "ascending") {
+			this.currentCards = sortObjectsByPropertyValue(this.currentCards, "age", "ascending");
+		}
+		if (sortType == "descending") {
+			this.currentCards = sortObjectsByPropertyValue(this.currentCards, "age", "descending");
+		}
+		this.renderCards();
+	}
+	filterBySearch(searchValue) {
+		const filteredArray = this.currentCards.filter((friendCard) => Object.values(friendCard).join(" ").toLowerCase().indexOf(searchValue.toLowerCase()) > -1);
+		this.renderCards(filteredArray);
 	}
 }
+
+const sortObjectsByPropertyValue = (arrayOfObjects, key, comparsionType) => {
+	const array = arrayOfObjects;
+	const modifier = comparsionType == "ascending" ? 1 : -1;
+	return array.sort((a, b) => (a[key] > b[key] ? 1 * modifier : a[key] < b[key] ? -1 * modifier : 0));
+};
 
 class FriendCard {
 	constructor(friendData) {
@@ -40,19 +125,16 @@ class FriendCard {
 	createCard() {
 		const card = document.createElement("div");
 		card.classList.add("card");
-		card.dataset.gender = this.gender;
 		card.innerHTML = `<img class="card__img" src="${this.photo}" alt="${this.name} photo"'>\n
                           <h3 class="card__name">${this.name}</h3>\n
                           <span class="card__age">Age: ${this.age}</span>\n
-                          <a class="card__phone" href="tel:+${this.phone.replace(/\D/g,"")}">${this.phone}</a>\n
+                          <a class="card__phone" href="tel:+${this.phone.replace(/\D/g, "")}">${this.phone}</a>\n
                           <a class="card__email" href="mailto:${this.email}">${this.email}</a>\n
                           <span class="card__country">${this.country}</span>`;
 		return card;
 	}
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-	const dataArray = await fetchData(REQUEST_LINK);
-	const friends = new FriendsList(dataArray, MAIN);
-	friends.renderCards();
+document.addEventListener("DOMContentLoaded", () => {
+	fetchData(REQUEST_LINK).then((results) => initApp(results));
 });
