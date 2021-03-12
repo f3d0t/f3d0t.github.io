8d19ce743a68fba96be6fe849f211292255e8b99
f3d0t <logrus7@gmail.com>
2021-03-12 07:23:40 +0300

    .

--- js-cinema/app.js
+++ js-cinema/app.js
@@ -2,25 +2,16 @@ const SEAT_PRICE = 2;
 const ACTIVE_SEATS = [];
 
 const FORM_BOOKING = document.querySelector(".form__booking");
-const FORM_TICKETS_NUMBER = document.querySelector(".form__tickets-number");
+const FORM_BUTTON = document.querySelector(".form__button");
+const FORM_TICKETS_COUNT = document.querySelector(".form__tickets-count");
 const FORM_TICKETS_COST = document.querySelector(".form__tickets-cost");
-const TICKETS_CONTAINER = document.querySelector(".tickets__cont");
-
-const initApp = () => {
-	bindEventListeners(FORM_BOOKING, TICKETS_CONTAINER, ACTIVE_SEATS, SEAT_PRICE);
-};
-
-const bindEventListeners = (formElement, ticketsContainer, seatsArray, seatPrice) => {
-	formElement.addEventListener("input", ({ target }) => {
-		if (target.checked === true) {
-			addSeat(seatsArray, target.dataset.row, target.dataset.seat);
-		} else if (target.checked === false) {
-			deleteSeat(seatsArray, target.dataset.row, target.dataset.seat);
-		}
-		updateForm(formElement, seatsArray, seatPrice);
-		updateTickets(ticketsContainer, seatsArray, seatPrice);
-	});
-};
+const TICKETS_CONTAINER = document.querySelector(".tickets_cont");
+const MODAL_TICKETS_CONTAINER = document.querySelector(".modal__tickets_cont");
+const MODAL_WRAPPER = document.querySelector(".modal_wrapper");
+const MODAL_FORM = document.querySelector(".modal");
+const MODAL_COUNT = document.querySelector(".modal__count");
+const MODAL_COST = document.querySelector(".modal__cost");
+const MODAL_CLOSE = document.querySelector(".modal__close");
 
 const addSeat = (seatsArray, row, seat) => {
 	seatsArray.push(`${row}-${seat}`);
@@ -30,19 +21,21 @@ const deleteSeat = (seatsArray, row, seat) => {
 	seatsArray.splice(seatsArray.indexOf(`${row}-${seat}`), 1);
 };
 
-const updateForm = (formElement, seatsArray, seatPrice) => {
+const updateForm = (formButton, formCount, formCost, seatsArray, seatPrice) => {
 	if (seatsArray.length > 0) {
-		FORM_TICKETS_NUMBER.textContent = seatsArray.length + " ticket";
-		if (seatsArray.length > 1) FORM_TICKETS_NUMBER.textContent += "s";
-		FORM_TICKETS_COST.textContent = " for " + seatsArray.length * seatPrice + "$";
+		formCount.textContent = seatsArray.length + " ticket";
+		if (seatsArray.length > 1) formCount.textContent += "s";
+		formCost.textContent = " for " + seatsArray.length * seatPrice + "$";
+		formButton.disabled = false;
 	} else {
-		FORM_TICKETS_NUMBER.textContent = "";
-		FORM_TICKETS_COST.textContent = "Select seats";
+		formCount.textContent = "";
+		formCost.textContent = "Please, select seats";
+		formButton.disabled = true;
 	}
 };
 
 const getTicketHtml = ([row, seat], price) => `
-    <div class="booked__tickets--ticket">
+    <div class="booked_tickets__ticket">
         <span class="ticket__row">Row: ${row}</span>
         <span class="ticket__seat">Seat: ${seat}</span>
         <span class="ticket__price">Price: ${price}$</span>
@@ -52,11 +45,55 @@ const getTicketsHtml = (seatsArray, seatPrice) => {
 	if (seatsArray.length > 0) {
 		return seatsArray.map((bookedSeat) => getTicketHtml(bookedSeat.split("-"), seatPrice)).join("");
 	}
-	return "<p>Select seats</p>";
+	return "<p>Select seats...=)</p>";
 };
 
 const updateTickets = (ticketsContainer, seatsArray, seatPrice) => {
 	ticketsContainer.innerHTML = getTicketsHtml(seatsArray, seatPrice);
+};
+
+const openModal = (modalWrapper, modalTicketsContainer, modalCount, modalCost, seatsArray, seatPrice) => {
+	updateTickets(modalTicketsContainer, seatsArray, seatPrice);
+	modalCount.textContent = seatsArray.length + " ticket";
+	if (seatsArray.length > 1) modalCount.textContent += "s";
+	modalCost.textContent = " for " + seatsArray.length * seatPrice + "$";
+	modalWrapper.style.display = "flex";
+};
+
+const closeModal = (modalWrapper) => {
+	modalWrapper.style.display = "none";
+};
+
+const initApp = () => {
+	bindEventListeners();
+};
+
+const bindEventListeners = () => {
+	FORM_BOOKING.addEventListener("input", ({ target }) => {
+		if (target.checked === true) {
+			addSeat(ACTIVE_SEATS, target.dataset.row, target.dataset.seat);
+		} else if (target.checked === false) {
+			deleteSeat(ACTIVE_SEATS, target.dataset.row, target.dataset.seat);
+		}
+		updateForm(FORM_BUTTON, FORM_TICKETS_COUNT, FORM_TICKETS_COST, ACTIVE_SEATS, SEAT_PRICE);
+		updateTickets(TICKETS_CONTAINER, ACTIVE_SEATS, SEAT_PRICE);
+	});
+	FORM_BOOKING.addEventListener("submit", () => {
+		openModal(MODAL_WRAPPER, MODAL_TICKETS_CONTAINER, MODAL_COUNT, MODAL_COST, ACTIVE_SEATS, SEAT_PRICE);
+	});
+	document.addEventListener("keydown", (event) => {
+		if (event.key === "Escape") {
+			event.preventDefault();
+			closeModal(MODAL_WRAPPER);
+		}
+	});
+	MODAL_CLOSE.addEventListener("click", () => closeModal(MODAL_WRAPPER));
+	MODAL_FORM.addEventListener("submit", () => {
+		alert("Sucsessfully booked!");
+		setTimeout(() => {
+			location.reload();
+		}, 1000);
+	});
 };
 
 document.addEventListener("DOMContentLoaded", () => {
